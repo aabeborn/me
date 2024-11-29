ca15f85214404c60d4a162cab6ff4150b7028da7
aabeborn <aabeborn@benato.dev>
2024-11-30 00:36:38 +0100

    🚧 Start building a basic landing page

---
+++ utils/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Indie_Flower } from 'next/font/google'
+
+export const inter = Inter({
+	subsets: ['latin'],
+	variable: '--inter-font'
+})
+
+export const indieFlower = Indie_Flower({
+	variable: '--indie-font',
+	subsets: ['latin'],
+	weight: '400'
+})
