aff6288494424a059a1d93e106719feb977ab33d
aabeborn <aabeborn@benato.dev>
2024-11-30 00:36:38 +0100

    🚧 Start building a basic landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,93 @@
+import Logo from '@/components/logo'
+import {
+	LinkedInLogoIcon,
+	TwitterLogoIcon,
+	InstagramLogoIcon,
+	GitHubLogoIcon
+} from '@radix-ui/react-icons'
+import { type Metadata } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
+import { Fragment } from 'react'
 
-export default function Home() {
+export const metadata: Metadata = {
+	title: 'Andrea Benato - Home'
+}
+
+const socials: {
+	Icon: typeof LinkedInLogoIcon
+	link: string
+	name: string
+}[] = [
+	{
+		Icon: LinkedInLogoIcon,
+		link: 'https://www.linkedin.com/in/andreabenato/',
+		name: 'LinkedIn'
+	},
+	{
+		Icon: GitHubLogoIcon,
+		link: 'https://github.com/aabeborn',
+		name: 'GitHub'
+	},
+	{
+		Icon: TwitterLogoIcon,
+		link: 'https://x.com/aabeborn',
+		name: 'Twitter'
+	},
+	{
+		Icon: InstagramLogoIcon,
+		link: 'https://www.instagram.com/aabeborn/',
+		name: 'Instagram'
+	}
+]
+
+const Page = () => {
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-				<Image
-					className="dark:invert"
-					src="/next.svg"
-					alt="Next.js logo"
-					width={180}
-					height={38}
-					priority
-				/>
-				<ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-					<li className="mb-2">
-						Get started by editing{' '}
-						<code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-							app/page.tsx
-						</code>
-						.
-					</li>
-					<li>Save and see your changes instantly.</li>
-				</ol>
-
-				<div className="flex gap-4 items-center flex-col sm:flex-row">
-					<a
-						className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Image
-							className="dark:invert"
-							src="/vercel.svg"
-							alt="Vercel logomark"
-							width={20}
-							height={20}
-						/>
-						Deploy now
-					</a>
-					<a
-						className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-						href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Read our docs
-					</a>
+		<div className="w-svw h-svh p-8 flex flex-col">
+			<div className="w-full inline-flex justify-center">
+				<Logo className="fill-primary-light w-24" />
+			</div>
+			<main className="flex flex-col grow items-center justify-center">
+				<div className="flex flex-col md:flex-row w-full max-w-[32rem] gap-8 items-center text-center md:text-start">
+					<Image
+						src="/me.jpg"
+						width={128}
+						height={128}
+						className="rounded-full"
+						alt="Andrea Benato"
+						quality={100}
+					/>
+					<div className="flex flex-col grow gap-4">
+						<span className=" font-display font-regular">
+							Ciao! I&apos;m{' '}
+							<span className="font-accent text-lg leading-1 font-bold hover:text-orange-400">
+								Andrea
+							</span>
+							, a frontend developer currently working as Frontend
+							Tech and Team leader at Deltatre in Italy
+						</span>
+						<div className="inline-flex gap-6 justify-center md:justify-start">
+							{socials.map(({ Icon, link, name }) => (
+								<Fragment key={name}>
+									<Link
+										key={name}
+										href={link}
+										target="_blank"
+									>
+										<Icon
+											width="1.5rem"
+											height="1.5rem"
+											className="text-primary hover:text-orange-400"
+										/>
+									</Link>
+								</Fragment>
+							))}
+						</div>
+					</div>
 				</div>
 			</main>
-			<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/file.svg"
-						alt="File icon"
-						width={16}
-						height={16}
-					/>
-					Learn
-				</a>
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/window.svg"
-						alt="Window icon"
-						width={16}
-						height={16}
-					/>
-					Examples
-				</a>
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/globe.svg"
-						alt="Globe icon"
-						width={16}
-						height={16}
-					/>
-					Go to nextjs.org →
-				</a>
-			</footer>
 		</div>
 	)
 }
+
+export default Page
