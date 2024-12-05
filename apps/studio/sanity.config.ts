b18cc7c65bbfa80177f35a8a508c920641882af1
aabeborn <aabeborn@benato.dev>
2024-12-05 21:50:30 +0100

    ✨ Sanity studio application

---
+++ apps/studio/sanity.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
+import { markdownSchema } from 'sanity-plugin-markdown'
+console.log(import.meta)
+if (
+	!import.meta.env.SANITY_STUDIO_PROJECT_ID ||
+	!import.meta.env.SANITY_STUDIO_DATASET
+)
+	throw new Error(
+		'Missing SANITY_STUDIO_PROJECT_ID or SANITY_STUDIO_STUDIO_DATASET in environment'
+	)
+
+export default defineConfig({
+	name: 'studio',
+	title: 'aabeborn - Studio',
+
+	projectId: import.meta.env.SANITY_STUDIO_PROJECT_ID,
+	dataset: import.meta.env.SANITY_STUDIO_DATASET,
+
+	plugins: [structureTool(), visionTool(), markdownSchema()],
+
+	schema: {
+		types: schemaTypes
+	}
+})
